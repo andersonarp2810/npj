de0575a08c27916c6c2ef577aee469fbcbd99d6c
Anderson Rodrigues Palmeira <andersonarp2810@gmail.com>
2018-10-04 15:09:00 -0300

    refactor comment controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,55 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
-use App\Entities\Comment;
-use App\Entities\DoubleStudent;
-use App\Entities\Group;
 use App\Entities\Human;
-use App\Entities\Petition;
-use App\Entities\Template;
-use Illuminate\Http\Request;
-use Auth;
-use Validator;
-
 use App\Services\AdminService;
+use App\User;
+use Auth;
+use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
 
-  public function __construct(AdminService $service){
-    $this->service = $service;
-  }
-
- /**
- * Display a listing of the resource.
- *
- * @return \Illuminate\Http\Response
- */
- public function index()
- {
-    if(Auth::user()->type != 'admin'){
-      return redirect()->back();
+    public function __construct(AdminService $service)
+    {
+        $this->service = $service;
     }
 
-    $dados = $this->service->index();
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if (Auth::user()->type != 'admin') {
+            return redirect()->back();
+        }
 
-     return view('admin.home')->with($dados);
- }
+        $dados = $this->service->index();
 
- public function preferences()
- {
-   $user  = User::find(Auth::user()->id);
-   $human = Human::where('user_id',$user->id)->first();
-   return view('admin.preferences')->with(['user'=>$user,'human'=>$human]);
- }
+        return view('admin.home')->with($dados);
+    }
 
- public function editar(Request $request)
- {
-   $user = User::find($request['idUser']);
-   $human = Human::find($request['idHuman']);
+    public function preferences()
+    {
+        $user = User::find(Auth::user()->id);
+        $human = Human::where('user_id', $user->id)->first();
+        return view('admin.preferences')->with(['user' => $user, 'human' => $human]);
+    }
 
-   return $this->service->editar($human, $user, $request);
- }
+    public function editar(Request $request)
+    {
+        $user = User::find($request['idUser']);
+        $human = Human::find($request['idHuman']);
+
+        return $this->service->editar($human, $user, $request);
+    }
 
 }
