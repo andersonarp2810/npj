8483a571e97c0dd77980adaccb3f8a0749961146
Franklin-Rodrigo <franklin@aluno.fapce.edu.br>
2018-10-10 16:53:58 -0300

    refactor(admin): component, Bottombar: todos os botoes funcionando

--- resources/views/components/bottombar.blade.php
+++ resources/views/components/bottombar.blade.php
@@ -6,31 +6,31 @@
     <ul class="navbar-nav mx-auto">
 
       <li class="nav-item ">
-        <a href="" class="nav-link text-center">
+        <a href="{{URL::to('Admin/Alunos')}}" class="nav-link text-center">
           <span class="fas fa-user fa-lg"></span>
           <small>Alunos</small>
         </a>
       </li>
       <li class="nav-item ">
-        <a href="" class="nav-link text-center">
+        <a href="{{URL::to('Admin/Duplas')}}" class="nav-link text-center">
           <span class="fas fa-user-friends fa-lg"></span>
           <small>Duplas</small>
         </a>
       </li>
       <li class="nav-item ">
-        <a href="" class="nav-link text-center">
+        <a href="{{URL::to('Admin/Professores')}}" class="nav-link text-center">
           <span class="fas fa-user-graduate fa-lg"></span>
           <small>Professores</small>
         </a>
       </li>
       <li class="nav-item ">
-        <a href="" class="nav-link">
+        <a href="{{URL::to('Admin/Grupos')}}" class="nav-link">
           <span class="fas fa-users fa-lg"></span>
           <small>Grupos</small>
         </a>
       </li>
       <li class="nav-item ">
-        <a href="" class="nav-link text-center">
+        <a href="{{URL::to('Admin/Defensores')}}" class="nav-link text-center">
           <span class="fas fa-user-tie fa-lg"></span>
           <small>Defensores</small>
         </a>
@@ -38,7 +38,7 @@
     </ul>
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
-        <a href="#" class="text-danger" onClick="location.href='{{URL::to('Sair')}}'">          
+        <a href="{{URL::to('Sair')}}" class="text-danger" onClick="location.href='{{URL::to('Sair')}}'">          
           <span class="fas fa-sign-out-alt fa-lg"></span>
           <small>Sair</small>
         </a>
