21a752ee6a9a1e3543c23ab5a8f0051ccfbaeb0b
Anderson Malheiro <malheiroanderson97@gmail.com>
2018-10-25 18:11:39 -0300

    refactor(Professor): Melhorias no layout

--- resources/views/teacher/templateEditar.blade.php
+++ resources/views/teacher/templateEditar.blade.php
@@ -7,15 +7,21 @@
         <form action="{{URL::to('Professor/Template/Editar')}}" method="post" enctype="multipart/form-data">
           {{ csrf_field() }}
           <input type="hidden" name="idTemplate" value="{{$template->id}}">
-          <label for="">TÍTULO</label>
+          <label for="">Título:</label>
           <input class="form-control" type="text" name="title" value="{{$template->title}}" required/>
-          <br><br>
-          <label for="">CONTEÚDO</label>
+          <br>
+          <label for="">Conteúdo:</label>
           <textarea  class="ckeditor" maxlength="99999" name="content" required>{{$template->content}}</textarea>
           <div class="row">
             <div class="modal-footer">
-              <button type="button" class="btn btn-danger" onClick="location.href='{{URL::to('Professor/Templates')}}'">VOLTAR</button>
-              <button type="submit" class="btn btn-primary">SALVAR</button>
+              <button type="button" class="btn btn-secondary" onClick="location.href='{{URL::to('Professor/Templates')}}'">
+                <span class="fas fa-arrow-left mr-2"></span>
+                Voltar
+              </button>
+              <button type="submit" class="btn btn-primary">
+                Salvar
+                <span class="fas fa-save"></span>
+              </button>
             </div>
           </div>
     </div>
--- resources/views/teacher/templateShow.blade.php
+++ resources/views/teacher/templateShow.blade.php
@@ -6,13 +6,18 @@
         <script src="{{ asset('tools/ckeditor/ckeditor.js')}}"></script>
         {{ csrf_field() }}
         <input type="hidden" name="idTemplate" value="{{$template->id}}">
-        <label for="">TÍTULO</label><br>
-        <input class="form-control mb-5" type="text" name="title" value="{{$template->title}}" disabled/>
-        <label for="">CONTEÚDO</label><br>
+        
+        <label for="">Título:</label><br>
+        <input class="form-control mb-3" type="text" name="title" value="{{$template->title}}" disabled/>
+        
+        <label for="">Conteúdo:</label><br>
         <textarea  class="ckeditor" maxlength="99999" name="content" disabled>{{$template->content}}</textarea>
         <div class="row">
           <div class="modal-footer">
-            <button type="button" class="btn btn-danger" onClick="location.href='{{URL::to('Professor/Templates')}}'">VOLTAR</button>
+            <button type="button" class="btn btn-secondary" onClick="location.href='{{URL::to('Professor/Templates')}}'">
+              <span class="fas fa-arrow-left mr-2"></span>
+              Voltar
+            </button>
           </div>
         </div>
     </div>
