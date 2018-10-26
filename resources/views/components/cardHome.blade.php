d9769f716b54ba8726a327eb37d15c9e3101459c
Gabriel Lima <gabriel.lima.rds@gmail.com>
2018-10-26 17:26:37 -0300

    refactor(Estudante): Refatoração da home do estudando com os components de card

--- resources/views/components/cardHome.blade.php
+++ resources/views/components/cardHome.blade.php
@@ -1,12 +1,12 @@
 <div class="card">
-    <div class="<?php echo('card-header '.$items['color'].' text-white') ?>">
+    <div class="<?php echo('card-header bg-'.$items['color'].' text-white') ?>">
         {{ $items['title'] }}
         <span class="<?php echo('fas '.$items['icon'].' fa-lg float-right my-1') ?>"></span>
     </div>
-    <div class="card-body text-center text-primary">
+    <div class="<?php echo('card-body text-center text-'.$items['color']) ?>">
         <h1 class="h1 h1-responsive">
             {{$slot}}
         </h1>
     </div>
-    <div class="<?php echo('card-footer '.$items['color']) ?>"></div>
+    <div class="<?php echo('card-footer bg-'.$items['color']) ?>"></div>
 </div>
--- resources/views/defender/home.blade.php
+++ resources/views/defender/home.blade.php
@@ -10,19 +10,19 @@
     <div class="row justify-content-center mt-3">
       
       <div class="col-lg-4 col-md-4 col-12 mb-3">
-        @cardHome(['items' => ['title' => 'ALUNO', 'icon' => 'fa-user', 'color' => 'bg-primary']])
+        @cardHome(['items' => ['title' => 'ALUNO', 'icon' => 'fa-user', 'color' => 'primary']])
           {{$petitions->where('defender_id',$defender->id)->where('student_ok','!=','true')->count()}}
         @endcardHome
       </div>
       
-      <div class="col-lg-4 col-md-4 col-12">
-        @cardHome(['items' => ['title' => 'DEFENSOR', 'icon' => 'fa-user-tie', 'color' => 'bg-primary']])  
+      <div class="col-lg-4 col-md-4 col-12 mb-3">
+        @cardHome(['items' => ['title' => 'DEFENSOR', 'icon' => 'fa-user-tie', 'color' => 'primary']])  
           {{$petitions->where('defender_id',$defender->id)->where('defender_ok','!=','true')->where('teacher_ok','true')->where('student_ok','true')->count()}}
         @endcardHome
       </div>
       
       <div class="col-lg-4 col-md-4 col-12">
-        @cardHome(['items' => ['title' => 'DISPONÍVEIS', 'icon' => 'fa-user-tie', 'color' => 'bg-primary']])  
+        @cardHome(['items' => ['title' => 'DISPONÍVEIS', 'icon' => 'fa-user-tie', 'color' => 'primary']])  
           {{$petitions->where('student_ok','true')->where('teacher_ok','true')->where('defender_ok','=','')->where('defender_id','=','')->count()}}
         @endcardHome
       </div>
@@ -31,13 +31,13 @@
     <div class="row justify-content-center mt-3">
       
       <div class="col-lg-4 col-md-6 mb-3">
-        @cardHome(['items' => ['title' => 'RECUSADAS', 'icon' => 'fa-user-graduate', 'color' => 'bg-danger']])  
+        @cardHome(['items' => ['title' => 'RECUSADAS', 'icon' => 'fa-user-graduate', 'color' => 'danger']])  
           {{$petitions->where('defender_id',$defender->id)->where('defender_ok','false')->count()}}
         @endcardHome
       </div>
       
       <div class="col-lg-4 col-md-6 mb-3">
-        @cardHome(['items' => ['title' => 'FINALIZADAS', 'icon' => 'fa-check', 'color' => 'bg-success']])  
+        @cardHome(['items' => ['title' => 'FINALIZADAS', 'icon' => 'fa-check', 'color' => 'success']])  
           {{$petitions->where('student_ok','true')->where('teacher_ok','true')->where('defender_ok','true')->where('defender_id',$defender->id)->count()}}
         @endcardHome
       </div>
