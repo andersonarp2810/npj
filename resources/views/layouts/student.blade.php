feae988eff39073b97c090883994dfd9ee055a2d
Anderson Rodrigues Palmeira <andersonarp2810@gmail.com>
2018-11-01 17:22:05 -0300

    fix(layouts): atraso no css era falta de @endsection

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <!-- Bootstrap -->
-<link rel="stylesheet" href="{{URL::asset('css/bootstrap4.css')}}">
 
 <!-- Sidebar -->
 @sidebar([
@@ -24,158 +23,153 @@
     {{Auth::user()->email}}  
   @endcomponent
   
-  
-  <!-- END Topbar -->
-  
-  <!-- Main container -->
-  <main>
-    <div>
-      @yield('component')
-    </div>
-  </main>
-    <!-- Footer -->
-    <footer class="mt-3">    
-      <nav class="navbar bottom navbar-light bg-light">
-        <ul class="navbar-nav mx-auto">
-          <li class="nav-item">
-            Copyright © 2018 NexTI.
-          </li>
-        </ul>
-      </nav>
-    </footer>
-    <!-- END Footer -->
-    @component('components.bottombar')  
-      {{Auth::user()->email}}  
-    @endcomponent
-  
-  <!-- END Main container -->
+
+<!-- END Topbar -->
+
+<!-- Main container -->
+<main>
+  <div>
+    @yield('component')
   </div>
-  
-  <script type="text/javascript">
+</main>
+<!-- Footer -->
+<footer class="mt-3">    
+  <nav class="navbar bottom navbar-light bg-light">
+    <ul class="navbar-nav mx-auto">
+      <li class="nav-item">
+        Copyright © 2018 NexTI.
+      </li>
+    </ul>
+  </nav>
+</footer>
+<!-- END Footer -->
+@component('components.bottombar')  
+  {{Auth::user()->email}}  
+@endcomponent
+
+<!-- END Main container -->
+</div>
+
+<script type="text/javascript">
 
 
-        function mudarPeticao(){
-          var id = $("#idPetition").val();
-          console.log(id);
+  function mudarPeticao(){
+    var id = $("#idPetition").val();
+    console.log(id);
 
-          $.post("{{URL::to('Aluno/Peticao/MudarPeticao')}}",{ 'id': id, '_token': $('meta[name=csrf-token]').attr('content') },function(response){
-            if(response != null){
-              location.reload();
-            }
-          });
+    $.post("{{URL::to('Aluno/Peticao/MudarPeticao')}}",{ 'id': id, '_token': $('meta[name=csrf-token]').attr('content') },function(response){
+      if(response != null){
+        location.reload();
+      }
+    });
+  }
+
+  function copiarPeticao(){
+    var id = $("#idPetition").val();
+    console.log(id);
+
+    $.post("{{URL::to('Aluno/Peticao/CopiarPeticao')}}",{ 'id': id, '_token': $('meta[name=csrf-token]').attr('content') },function(response){
+      if(response != null){
+        location.reload();
+      }
+    });
+  }
+
+
+  function deleteStudent(id,nome) {
+    document.getElementById('deleteIdStudent').value = id;
+    document.getElementById('deleteNomeStudent').innerHTML = nome;
+  }
+
+  function editStudent(id,name,email,gender,age,phone,password){
+    $('#studentId').val(id);
+    $('#studentName').val(name);
+    $('#studentEmail').val(email);
+    $('#studentGender').val(gender);
+    $('#studentAge').val(age);
+    $('#studentPhone').val(phone);
+    $('#studentPassword').val(password);
+  }
+
+
+  function editPetition(id,description,content,template_id,defender_id,doubleStudent_id){
+    $('#petitionId').val(id);
+    $('#petitionDescription').val(description);
+    $('#petitionContent').val(content);
+    $('#petitionTemplate_id').val(template_id);
+    $('#petitionDefender_id').val(defender_id);
+    $('#petitionDoubleStudent_id').val(doubleStudent_id);
+  }
+
+
+  function deleteTeacher(id,nome) {
+    document.getElementById('deleteIdTeacher').value = id;
+    document.getElementById('deleteNomeTeacher').innerHTML = nome;
+  }
+
+  function editTeacher(id,name,email,gender,age,phone,password){
+    $('#teacherId').val(id);
+    $('#teacherName').val(name);
+    $('#teacherEmail').val(email);
+    $('#teacherGender').val(gender);
+    $('#teacherAge').val(age);
+    $('#teacherPhone').val(phone);
+    $('#teacherPassword').val(password);
+  }
+
+  function deleteDefender(id,nome) {
+    document.getElementById('deleteIdDefender').value = id;
+    document.getElementById('deleteNomeDefender').innerHTML = nome;
+  }
+
+  function editDefender(id,name,email,gender,age,phone,password){
+    $('#defenderId').val(id);
+    $('#defenderName').val(name);
+    $('#defenderEmail').val(email);
+    $('#defenderGender').val(gender);
+    $('#defenderAge').val(age);
+    $('#defenderPhone').val(phone);
+    $('#defenderPassword').val(password);
+  }
+
+
+  function deleteGroup(id,nome) {
+    document.getElementById('deleteIdGroup').value = id;
+    document.getElementById('deleteNameGroup').innerHTML = nome;
+  }
+
+  function editModalGroup(id,name,teacher_id){
+    $('#groupName').val(name);
+    $('#groupId').val(id);
+    $('#groupTeacher_id').val(teacher_id);
+  }
+
+  function deletePetition(id,description){
+    document.getElementById('deleteIdPetition').value = id;
+    document.getElementById('deleteDescriptionPetition').innerHTML = description;
+  }
+
+
+  function filtroDeBusca(nome){
+    var objs = document.getElementsByClassName("object");
+
+    $('.object').show();
+
+    if (nome != "") {
+      for (var i = 0; i < objs.length; i++) {
+        if (objs[i].getAttribute('name').toLowerCase().search(nome.toLowerCase())) {
+          objs[i].style.display = "none";
         }
+      }
+    }
+  }
 
-        function copiarPeticao(){
-          var id = $("#idPetition").val();
-          console.log(id);
+  function escolherTemplate(idTemplate){
 
-          $.post("{{URL::to('Aluno/Peticao/CopiarPeticao')}}",{ 'id': id, '_token': $('meta[name=csrf-token]').attr('content') },function(response){
-            if(response != null){
-              location.reload();
-            }
-          });
-        }
+    $("#step0").hide().fadeOut();
+    $("#formCadastro").show().fadeIn();
+    $("#btnCadastrar").show();
+  }
 
-
-        function deleteStudent(id,nome) {
-          document.getElementById('deleteIdStudent').value = id;
-          document.getElementById('deleteNomeStudent').innerHTML = nome;
-        }
-
-        function editStudent(id,name,email,gender,age,phone,password){
-          $('#studentId').val(id);
-          $('#studentName').val(name);
-          $('#studentEmail').val(email);
-          $('#studentGender').val(gender);
-          $('#studentAge').val(age);
-          $('#studentPhone').val(phone);
-          $('#studentPassword').val(password);
-        }
-
-
-        function editPetition(id,description,content,template_id,defender_id,doubleStudent_id){
-          $('#petitionId').val(id);
-          $('#petitionDescription').val(description);
-          $('#petitionContent').val(content);
-          $('#petitionTemplate_id').val(template_id);
-          $('#petitionDefender_id').val(defender_id);
-          $('#petitionDoubleStudent_id').val(doubleStudent_id);
-        }
-
-
-        function deleteTeacher(id,nome) {
-          document.getElementById('deleteIdTeacher').value = id;
-          document.getElementById('deleteNomeTeacher').innerHTML = nome;
-        }
-
-        function editTeacher(id,name,email,gender,age,phone,password){
-          $('#teacherId').val(id);
-          $('#teacherName').val(name);
-          $('#teacherEmail').val(email);
-          $('#teacherGender').val(gender);
-          $('#teacherAge').val(age);
-          $('#teacherPhone').val(phone);
-          $('#teacherPassword').val(password);
-        }
-
-        function deleteDefender(id,nome) {
-          document.getElementById('deleteIdDefender').value = id;
-          document.getElementById('deleteNomeDefender').innerHTML = nome;
-        }
-
-        function editDefender(id,name,email,gender,age,phone,password){
-          $('#defenderId').val(id);
-          $('#defenderName').val(name);
-          $('#defenderEmail').val(email);
-          $('#defenderGender').val(gender);
-          $('#defenderAge').val(age);
-          $('#defenderPhone').val(phone);
-          $('#defenderPassword').val(password);
-        }
-
-
-        function deleteGroup(id,nome) {
-          document.getElementById('deleteIdGroup').value = id;
-          document.getElementById('deleteNameGroup').innerHTML = nome;
-        }
-
-        function editModalGroup(id,name,teacher_id){
-          $('#groupName').val(name);
-          $('#groupId').val(id);
-          $('#groupTeacher_id').val(teacher_id);
-        }
-
-        function deletePetition(id,description){
-          document.getElementById('deleteIdPetition').value = id;
-          document.getElementById('deleteDescriptionPetition').innerHTML = description;
-        }
-
-
-        function filtroDeBusca(nome){
-          var objs = document.getElementsByClassName("object");
-
-          $('.object').show();
-
-          if (nome != "") {
-            for (var i = 0; i < objs.length; i++) {
-              if (objs[i].getAttribute('name').toLowerCase().search(nome.toLowerCase())) {
-                objs[i].style.display = "none";
-              }
-            }
-          }
-        }
-
-        function escolherTemplate(idTemplate){
-
-          $("#step0").hide().fadeOut();
-          $("#formCadastro").show().fadeIn();
-          $("#btnCadastrar").show();
-        }
-
-
-
-//
-
-    </script>
-</body>
-</html>
+</script>
+@endsection
--- resources/views/layouts/teacher.blade.php
+++ resources/views/layouts/teacher.blade.php
@@ -1,186 +1,183 @@
+@extends('layouts.base')
 
-  @extends('layouts.base')
-  
-  @section('content')
-  <!-- Bootstrap -->
-  <link rel="stylesheet" href="{{URL::asset('css/bootstrap4.css')}}">
-  
-  <!-- Sidebar -->
-  @sidebar([
-      'url' => "Professor",
-      'items' => [
-         
-          [
-              'item' => 'Duplas', 'itemUrl' => '/Professor/Duplas', 'icon' => 'user-friends'
-          ],
-          [
-              'item' => 'Petições', 'itemUrl' => '/Professor/Peticoes', 'icon' => 'file-alt'
-          ],
-          [
-              'item' => 'Templates', 'itemUrl' => '/Professor/Templates', 'icon' => 'file'
-          ],
+@section('content')
+<!-- Bootstrap -->
+
+<!-- Sidebar -->
+@sidebar([
+    'url' => "Professor",
+    'items' => [
         
-      ]
-  ])
-  @endsidebar
-  
-  <!-- END Sidebar -->
-  
-  
-  <div id="main">
-  <!-- Topbar -->
-  @component('components.topbar')  
+        [
+            'item' => 'Duplas', 'itemUrl' => '/Professor/Duplas', 'icon' => 'user-friends'
+        ],
+        [
+            'item' => 'Petições', 'itemUrl' => '/Professor/Peticoes', 'icon' => 'file-alt'
+        ],
+        [
+            'item' => 'Templates', 'itemUrl' => '/Professor/Templates', 'icon' => 'file'
+        ],
+      
+    ]
+])
+@endsidebar
+
+<!-- END Sidebar -->
+
+
+<div id="main">
+<!-- Topbar -->
+@component('components.topbar')  
+  {{Auth::user()->email}}  
+@endcomponent
+
+
+<!-- END Topbar -->
+
+<!-- Main container -->
+<main>
+  <div>
+    @yield('component')
+  </div>
+</main>
+  <!-- Footer -->
+  <footer class="mt-3">    
+    <nav class="navbar bottom navbar-light bg-light">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          Copyright © 2018 NexTI.
+        </li>
+      </ul>
+    </nav>
+  </footer>
+  <!-- END Footer -->
+  @component('components.bottombar')  
     {{Auth::user()->email}}  
   @endcomponent
-  
-  
-  <!-- END Topbar -->
-  
-  <!-- Main container -->
-  <main>
-    <div>
-      @yield('component')
-    </div>
-  </main>
-    <!-- Footer -->
-    <footer class="mt-3">    
-      <nav class="navbar bottom navbar-light bg-light">
-        <ul class="navbar-nav mx-auto">
-          <li class="nav-item">
-            Copyright © 2018 NexTI.
-          </li>
-        </ul>
-      </nav>
-    </footer>
-    <!-- END Footer -->
-    @component('components.bottombar')  
-      {{Auth::user()->email}}  
-    @endcomponent
-  
-  <!-- END Main container -->
-  </div>
 
-  <!-- Scripts -->
-  <script type="text/javascript">
+<!-- END Main container -->
+</div>
 
-        //funcao da PAGINA home
-        function verifica(value){
-          if(value == 1){
-            document.getElementById('option1').style.display = "block";
-            document.getElementById('option2').style.display = "none";
-          }else if(value == 2){
-            document.getElementById('option1').style.display = "none";
-            document.getElementById('option2').style.display = "block";
-          }
+<!-- Scripts -->
+<script type="text/javascript">
+
+      //funcao da PAGINA home
+      function verifica(value){
+        if(value == 1){
+          document.getElementById('option1').style.display = "block";
+          document.getElementById('option2').style.display = "none";
+        }else if(value == 2){
+          document.getElementById('option1').style.display = "none";
+          document.getElementById('option2').style.display = "block";
         }
+      }
 
 
-        var comment = document.getElementById("comment");
-        var btnReprovar = document.getElementById("btnReprovar");
+      var comment = document.getElementById("comment");
+      var btnReprovar = document.getElementById("btnReprovar");
 
-        var onBriefingInput = function (event) {
-          btnReprovar.disabled = !event.target.value;
-        }
+      var onBriefingInput = function (event) {
+        btnReprovar.disabled = !event.target.value;
+      }
 
-        comment.addEventListener("input", onBriefingInput);
-        comment.dispatchEvent(new Event('input'));
-
-
-        function deleteStudent(id,nome) {
-          document.getElementById('deleteIdStudent').value = id;
-          document.getElementById('deleteNomeStudent').innerHTML = nome;
-        }
-
-        function editStudent(id,name,email,gender,age,phone,password){
-          $('#studentId').val(id);
-          $('#studentName').val(name);
-          $('#studentEmail').val(email);
-          $('#studentGender').val(gender);
-          $('#studentAge').val(age);
-          $('#studentPhone').val(phone);
-          $('#studentPassword').val(password);
-        }
+      comment.addEventListener("input", onBriefingInput);
+      comment.dispatchEvent(new Event('input'));
 
 
-        function deleteTeacher(id,nome) {
-          document.getElementById('deleteIdTeacher').value = id;
-          document.getElementById('deleteNomeTeacher').innerHTML = nome;
-        }
+      function deleteStudent(id,nome) {
+        document.getElementById('deleteIdStudent').value = id;
+        document.getElementById('deleteNomeStudent').innerHTML = nome;
+      }
 
-        function editTeacher(id,name,email,gender,age,phone,password){
-          $('#teacherId').val(id);
-          $('#teacherName').val(name);
-          $('#teacherEmail').val(email);
-          $('#teacherGender').val(gender);
-          $('#teacherAge').val(age);
-          $('#teacherPhone').val(phone);
-          $('#teacherPassword').val(password);
-        }
-
-        function deleteDefender(id,nome) {
-          document.getElementById('deleteIdDefender').value = id;
-          document.getElementById('deleteNomeDefender').innerHTML = nome;
-        }
-
-        function editDefender(id,name,email,gender,age,phone,password){
-          $('#defenderId').val(id);
-          $('#defenderName').val(name);
-          $('#defenderEmail').val(email);
-          $('#defenderGender').val(gender);
-          $('#defenderAge').val(age);
-          $('#defenderPhone').val(phone);
-          $('#defenderPassword').val(password);
-        }
+      function editStudent(id,name,email,gender,age,phone,password){
+        $('#studentId').val(id);
+        $('#studentName').val(name);
+        $('#studentEmail').val(email);
+        $('#studentGender').val(gender);
+        $('#studentAge').val(age);
+        $('#studentPhone').val(phone);
+        $('#studentPassword').val(password);
+      }
 
 
-        function deleteGroup(id,nome) {
-          document.getElementById('deleteIdGroup').value = id;
-          document.getElementById('deleteNameGroup').innerHTML = nome;
-        }
+      function deleteTeacher(id,nome) {
+        document.getElementById('deleteIdTeacher').value = id;
+        document.getElementById('deleteNomeTeacher').innerHTML = nome;
+      }
 
-        function editModalGroup(id,name,teacher_id){
-          $('#groupName').val(name);
-          $('#groupId').val(id);
-          $('#groupTeacher_id').val(teacher_id);
-        }
+      function editTeacher(id,name,email,gender,age,phone,password){
+        $('#teacherId').val(id);
+        $('#teacherName').val(name);
+        $('#teacherEmail').val(email);
+        $('#teacherGender').val(gender);
+        $('#teacherAge').val(age);
+        $('#teacherPhone').val(phone);
+        $('#teacherPassword').val(password);
+      }
 
-        function deletedoubleStudent(id){
-          document.getElementById('deleteIdDoubleStudent').value = id;
-        }
+      function deleteDefender(id,nome) {
+        document.getElementById('deleteIdDefender').value = id;
+        document.getElementById('deleteNomeDefender').innerHTML = nome;
+      }
 
-        function editModaldoubleStudent(id,student_id,student2_id,group_id){
-          $('#doubleStudentId').val(id);
-          $('#doubleStudentStudent_id').val(student_id);
-          $('#doubleStudentStudent2_id').val(student2_id);
-          $('#doubleStudentGroup_id').val(group_id);
-        }
+      function editDefender(id,name,email,gender,age,phone,password){
+        $('#defenderId').val(id);
+        $('#defenderName').val(name);
+        $('#defenderEmail').val(email);
+        $('#defenderGender').val(gender);
+        $('#defenderAge').val(age);
+        $('#defenderPhone').val(phone);
+        $('#defenderPassword').val(password);
+      }
 
 
-        function filtroDeBusca(nome){
-          var objs = document.getElementsByClassName("object");
+      function deleteGroup(id,nome) {
+        document.getElementById('deleteIdGroup').value = id;
+        document.getElementById('deleteNameGroup').innerHTML = nome;
+      }
 
-          $('.object').show();
+      function editModalGroup(id,name,teacher_id){
+        $('#groupName').val(name);
+        $('#groupId').val(id);
+        $('#groupTeacher_id').val(teacher_id);
+      }
 
-          if (nome != "") {
-            for (var i = 0; i < objs.length; i++) {
-              if (objs[i].getAttribute('name').toLowerCase().search(nome.toLowerCase())) {
-                objs[i].style.display = "none";
-              }
+      function deletedoubleStudent(id){
+        document.getElementById('deleteIdDoubleStudent').value = id;
+      }
+
+      function editModaldoubleStudent(id,student_id,student2_id,group_id){
+        $('#doubleStudentId').val(id);
+        $('#doubleStudentStudent_id').val(student_id);
+        $('#doubleStudentStudent2_id').val(student2_id);
+        $('#doubleStudentGroup_id').val(group_id);
+      }
+
+
+      function filtroDeBusca(nome){
+        var objs = document.getElementsByClassName("object");
+
+        $('.object').show();
+
+        if (nome != "") {
+          for (var i = 0; i < objs.length; i++) {
+            if (objs[i].getAttribute('name').toLowerCase().search(nome.toLowerCase())) {
+              objs[i].style.display = "none";
             }
           }
-
         }
 
-
-        function mudarStatusTemplate(id){
-          $.post("{{URL::to('Professor/Template/Status')}}",{ 'id': id, '_token': $('meta[name=csrf-token]').attr('content') },function(response){
-            if(response != null){
-              location.reload();
-            }
-          });
-        }
+      }
 
 
-      </script>
-</body>
-</html>
+      function mudarStatusTemplate(id){
+        $.post("{{URL::to('Professor/Template/Status')}}",{ 'id': id, '_token': $('meta[name=csrf-token]').attr('content') },function(response){
+          if(response != null){
+            location.reload();
+          }
+        });
+      }
+
+
+    </script>
+@endsection
